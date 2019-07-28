69e7a70fb42ee8965495803d87d0a2cbdc2e61fd
jipara <zhypargul.esengulova@gmail.com>
2019-07-28 16:52:58 -0500

    key

--- instance.tf
+++ instance.tf
@@ -2,4 +2,14 @@ resource "aws_instance" "web1" {
   ami           = "ami-02f706d959cedf892"
   instance_type = "t2.micro"
   key-name      = "terraform_april"
+
+
+
+
+  tags = {
+    Name       = "Dev"
+    Dept       = "IT"
+    Group      = "April"
+    Created_by = "Jipara"
+  }
 }
