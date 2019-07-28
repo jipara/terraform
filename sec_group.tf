a10895f32f8c783e659bcd18a65f0d82d37b24d9
jipara <zhypargul.esengulova@gmail.com>
2019-07-28 16:42:03 -0500

    add group

--- sec_group.tf
+++ sec_group.tf
@@ -5,10 +5,17 @@ resource "aws_security_group" "sec-1" {
 
   ingress {
     # TLS (change to whatever ports you need)
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
