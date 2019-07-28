2537909cd59e4b2fa4da09a43db918f648cfd7cc
jipara <zhypargul.esengulova@gmail.com>
2019-07-28 16:57:21 -0500

    hhhh

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web1" {
   ami           = "ami-02f706d959cedf892"
   instance_type = "t2.micro"
-  key-name      = "terraform_april"
+  key_name      = "terraform_april"
 
 
 
