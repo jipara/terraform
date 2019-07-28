1fba2b44fe17a5b235c0a557e0d0c728f80a58e2
jipara <zhypargul.esengulova@gmail.com>
2019-07-28 16:38:44 -0500

    jjjj

--- sec_group.tf
+++ sec_group.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "sec-1" {
     # TLS (change to whatever ports you need)
     from_port   = 22
     to_port     = 22
-    protocol    = "-1"
+    protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
     cidr_blocks = ["0.0.0.0/0"]
