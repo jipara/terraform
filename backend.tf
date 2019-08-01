92f0b5c7d8283c52d9f56b964ee0b174f7a1dc0e
jipara <zhypargul.esengulova@gmail.com>
2019-07-31 20:37:40 -0500

    add

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    dynamodb_table = "dev_state_lock"
-    bucket         = "terraform-april-2019-jiapra"
-    region         = "us-east-2"
-    key            = "dev.tfstate"
+
+    bucket = "terraform-april-2019-jiapra"
+    region = "us-east-2"
+    key    = "dev.tfstate"
   }
 }
