d277596fc52f28f2d041f93758896fa508b3aabd
jipara <zhypargul.esengulova@gmail.com>
2019-07-31 20:02:26 -0500

    aaaa

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
-    backend = "s3" {
-        bucket = "terraform-april-2019-jiapra"
-        region = "us-east-2"
-        key = "dev.tfstate"
-    }
+  backend "s3" {
+    bucket = "terraform-april-2019-jiapra"
+    region = "us-east-2"
+    key    = "dev.tfstate"
+  }
 }
